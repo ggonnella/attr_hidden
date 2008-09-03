5c39f53841b9f441e9c0ffe42d5869ed9a66a275
Giorgio Gonnella <ggonnella@yahoo.it>
2008-09-03 16:02:59 +0200

    added comment on the new feature
     which I should later transform in an unit test,
     as soon as I have learnt how to correctly do unit
     tests in active record plugins :)
    
     I currently test it in a "mock" rails application
     (tests not included in this plugin)

--- lib/active_record/attr_hidden.rb
+++ lib/active_record/attr_hidden.rb
@@ -47,6 +47,29 @@ module ActiveRecord::AttrHidden
       # this behaviour hiding/unhiding can be then overridden in single subclasses
       # calling attr_hidden / attr_unhidden as appropriate
       #
+      # example usage 
+      # table column list: a1, a2, a3
+      #
+      # class A; attr_hidden :a1; end          => hidden a1      / visible a2, a3
+      # class B<A; attr_hidden :a2; end        => hidden a1, a2  / visible a3
+      # class D<B; attr_not_hidden; :a2; end   => hidden a1      / visible a2, a3
+      # class C<B; end                         => hidden a1, a2  / visible a3
+      #
+      # class D<A; attr_hidden_in_subclasses :a3; end   => hidden a1, a2 / visible a3, a4
+      # class E<D; end                                  => hidden a1, a2, a3 / visible a4
+      # 
+      # * known issue of the current implementation: * 
+      #
+      # that overrides of ..._in_subclasses are by default not inherited in its subclasses
+      #
+      # class F<D; attr_not_hidden :a3; end             => hidden a1, a2 / visible a3, a4
+      # class G<F; end                                  => hidden a1, a2, a3 / visible a4 (!!!!)
+      #
+      # the inheritability must be explicitly specified this way: 
+      #
+      # class H<D; attr_not_hidden :a3; attr_not_hidden_in_subclasses; end    => hidden a1, a2 / visible a3, a4
+      # class I<H; end                                                        => hidden a1, a2 / visible a3, a4
+      #
       [:attr_hidden, :attr_not_hidden].each do |macro|
         define_method "#{macro}_in_subclasses" do |*attrs|
           (class << self; self; end).class_eval do
